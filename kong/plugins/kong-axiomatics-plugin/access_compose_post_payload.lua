9c93d22318fb22908ce57d7c93d2e5cdffa872f8
Jonas Iggbom <jonas.iggbom@axiomatics.com>
2019-10-06 21:44:31 -0700

    Updated request builder to map resource path to resourceType attribute and query param id to recordId attribute as well as the access method (GET) to action attribute

--- kong/plugins/kong-axiomatics-plugin/access_compose_post_payload.lua
+++ kong/plugins/kong-axiomatics-plugin/access_compose_post_payload.lua
@@ -20,30 +20,57 @@ function _M.compose_post_payload(decoded_token, conf)
     end
   end
   -- AccessSubject XACML values from URI path
-  if conf.url_parameter_matching then
-    local key_value = {}
-    key_value["AttributeId"] = conf.url_parameter_key
-    key_value["Value"] = string.match(ngx.var.request_uri, "([^/]+)$")
-    table.insert(access_subject_attribute_list, key_value)
-  end
+  -- if conf.url_parameter_matching then
+  --   local key_value = {}
+  --   key_value["AttributeId"] = conf.url_parameter_key
+  --   key_value["Value"] = string.match(ngx.var.request_uri, "([^/]+)$")
+  --   local message = "KEY VALUE" + key_value
+  --   table.insert(access_subject_attribute_list, key_value)
+  -- end
 
   access_subject["Attribute"] = access_subject_attribute_list
 
   -- Action XACML values
   local action_attribute = {}
   local action = {}
-  action_attribute["AttributeId"] = "action-id"
-  action_attribute["Value"] = ngx.var.scheme .. "://" .. ngx.var.host .. ngx.var.request_uri
-  action_attribute["DataType"] = "anyURI"
+  -- action_attribute["AttributeId"] = "action-id"
+  action_attribute["AttributeId"] = "axiomatics.demo.actionId"
+  -- action_attribute["Value"] = ngx.var.scheme .. "://" .. ngx.var.host .. ngx.var.request_uri
+  local a = kong.request.get_method()
+
+  if kong.request.get_method() == "GET" then
+    kong.log.info("The method is: ", kong.request.get_method(), ". Setting action to view")
+    action_attribute["Value"] = "view"
+
+  end
+  -- action_attribute["DataType"] = "anyURI"
   action["Attribute"] = action_attribute
 
   -- Resource XACML values TODO
+  local resource_attribute_list = {}
+  local resource_attribute = {}
+  local resource = {}
+  resource_attribute["AttributeId"] = "axiomatics.demo.resourceType"
+  local p = string.sub(kong.request.get_path(), 2)
+  kong.log.info("The resource path is: ", string.sub(kong.request.get_path())
+  -- resource_attribute["Value"] = "record"
+  resource_attribute["Value"] = p
+  table.insert(resource_attribute_list, resource_attribute)
+  resource_attribute = {}
+  resource_attribute["AttributeId"] = "axiomatics.demo.record.recordId"
+  -- resource_attribute["Value"] = "123"
+  local resource_id = kong.request.get_query_arg("id")
+  kong.log.info("The resource id is: ", resource_id)
+  resource_attribute["Value"] = resource_id
+  table.insert(resource_attribute_list, resource_attribute)
+  resource["Attribute"] = resource_attribute_list
 
   -- Payload
-  request = {}
-  payload = {}
+  local request = {}
+  local payload = {}
   request["AccessSubject"] = access_subject
   request["Action"] = action
+  request["Resource"] = resource
   payload["Request"] = request
 
   return JSON:encode_pretty(payload)
