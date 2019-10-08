9fea5b5363ace89b52999a2ecd6ca1b2103b7825
Jonas Iggbom <jonas.iggbom@axiomatics.com>
2019-10-07 18:52:02 -0700

    Fixed issue with path in script adding custom plugin

--- addCustomPlugin.sh
+++ addCustomPlugin.sh
@@ -3,7 +3,7 @@ PLUGINS="bundled"
 # mkdir tmp
 # cd tmp
 # git clone https://github.com/iggbom/kong-axiomatics-plugin
-docker cp kong-axiomatics-plugin/kong/plugins/kong-axiomatics-plugin/  kong:/usr/local/share/lua/5.1/kong/plugins
+docker cp ./kong/plugins/kong-axiomatics-plugin/  kong:/usr/local/share/lua/5.1/kong/plugins
 docker exec -ti kong /bin/sh -c "KONG_PLUGINS='bundled,kong-axiomatics-plugin' kong reload"
 # cd ..
 # rm -Rf tmp
